a2e9b17918d993aebf492a676cbfee28311005e1
Gustavo <gustavocars@gmail.com>
2022-09-19 18:41:20 -0300

    Ocultado datos personales.

--- Extraer menor valor de lista 2.py
+++ Extraer menor valor de lista 2.py
@@ -3,7 +3,7 @@
 Created on Fri Nov 12 10:37:47 2021
 
 Nombre: Gustavo Reyes Herrera
-RUT: 21023531-0
+RUT: oculto
 
 Mis respuestas en este trabajo son propias, y están realizadas
 en conformidad con la formación ética de la universidad.
@@ -12,15 +12,18 @@ en conformidad con la formación ética de la universidad.
 
 # Este script determinará el menor valor dentro de una lista con una función y mostrará el resultado en pantalla.
 
+
 def menor_valor(lista):
     menor = lista[0]
-    for i in range(1, len(lista)): # lista[1:len(lista+1)]
+    for i in range(1, len(lista)):  # lista[1:len(lista+1)]
         if lista[i] < menor:
             menor = lista[i]
             indice = i
     return menor, indice
 
+
 lista = [5, 8, 3, 1, 9, 0, 3, 6, 1, 8]
 menor, indice_en_lista = menor_valor(lista)
 
-print(f"El menor valor de la lista es: {menor} y su índice en la lista es {indice_en_lista}")
+print(
+    f"El menor valor de la lista es: {menor} y su índice en la lista es {indice_en_lista}")
--- Extraer menor valor de lista.py
+++ Extraer menor valor de lista.py
@@ -3,7 +3,7 @@
 Created on Tue Nov  9 10:43:50 2021
 
 Nombre: Gustavo Reyes Herrera
-RUT: 21023531-0
+RUT: oculto
 
 Mis respuestas en este trabajo son propias, y están realizadas
 en conformidad con la formación ética de la universidad.
@@ -11,12 +11,14 @@ en conformidad con la formación ética de la universidad.
 """
 # Este script determinará el menor valor dentro de una lista con una función y mostrará el resultado en pantalla.
 
+
 def menor_valor(lista):
     menor = lista[0]
-    for i in lista[1:]: # lista[1:len(lista+1)]
+    for i in lista[1:]:  # lista[1:len(lista+1)]
         if i < menor:
             menor = i
     return menor
+
 
 lista = [5, 8, 3, 1, 9, 0, 3, 6, 1, 8]
 menor = menor_valor(lista)
